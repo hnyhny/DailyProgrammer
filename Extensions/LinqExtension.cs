e08c077288d2c47775a97f0adc2cb235601cfb20
hnyhny <none@giv.en>
2019-07-09 16:50:05 +0200

    Adds Each method for foreach with index

--- Extensions/LinqExtension.cs
+++ Extensions/LinqExtension.cs
@@ -24,5 +24,10 @@ namespace hnyhny.Extensions
                 }
             }
         }
+        public static void Each<T>(this IEnumerable<T> ie, Action<T, int> action)
+        {
+            var i = 0;
+            foreach (var e in ie) action(e, i++);
+        }
     }
 }
