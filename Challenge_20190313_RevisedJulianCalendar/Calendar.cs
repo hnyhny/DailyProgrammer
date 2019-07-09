3c8c0b180993414a2f3f42cfb557deaf2215d4fb
hnyhny <none@giv.en>
2019-07-09 13:12:41 +0200

    Optimized for bigger numbers

--- Challenge_20190313_RevisedJulianCalendar/Calendar.cs
+++ Challenge_20190313_RevisedJulianCalendar/Calendar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 
 namespace hnyhny.Challenges.RevisedJulianCalendar
@@ -8,8 +9,27 @@ namespace hnyhny.Challenges.RevisedJulianCalendar
         {
             return Enumerable.Range(startYear, endYear - startYear).Where(HasLeapDay).Count();
         }
+        internal long CountLeapDays(long startYear, long endYear)
+        {           
+            if(startYear == endYear)
+                return 0;
 
+            var leapsInIntervall = (endYear - startYear) / 4
+            - (endYear - startYear) / 100
+            + ((endYear - 200) / 900) - ((startYear - 200) / 900)
+            + ((endYear - 600) / 900) - ((startYear - 600) / 900);
+
+            if(HasLeapDay(startYear))
+                leapsInIntervall += 1;
+            if(endYear - startYear > 900)
+                leapsInIntervall += 178;
+            return leapsInIntervall;
+        }
         private bool HasLeapDay(int year)
+        {
+            return HasLeapDay((long)year);
+        }
+        private bool HasLeapDay(long year)
         {
             if (year % 900 == 200 || year % 900 == 600)
                 return true;
--- Challenge_20190313_RevisedJulianCalendar/RevisedJulianCalendarTest.cs
+++ Challenge_20190313_RevisedJulianCalendar/RevisedJulianCalendarTest.cs
@@ -16,7 +16,23 @@ namespace hnyhny.Challenges.RevisedJulianCalendar
         [InlineData(123456, 123456, 0)]
         [InlineData(1234, 5678, 1077)]
         [InlineData(123456, 7891011, 1881475)]
-        public void TestCountLeapDays(int startYear, int endYear, int expected)
+        public void TestCountLeapDaysInteger(int startYear, int endYear, int expected)
+        {
+            var actual = new Calendar().CountLeapDays(startYear, endYear);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(2016, 2017, 1)]
+        [InlineData(2019, 2020, 0)]
+        [InlineData(1900, 1901, 0)]
+        [InlineData(2000, 2001, 1)]
+        [InlineData(2800, 2801, 0)]
+        [InlineData(123456, 123456, 0)]
+        [InlineData(1234, 5678, 1077)]
+        [InlineData(123456, 7891011, 1881475)]
+        [InlineData(123456789101112, 1314151617181920, 288412747246240)]
+        public void TestCountLeapDaysUnsignedLong(long startYear, long endYear, long expected)
         {
             var actual = new Calendar().CountLeapDays(startYear, endYear);
             Assert.Equal(expected, actual);
